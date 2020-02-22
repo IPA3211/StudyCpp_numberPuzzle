37f8b660c5225faf3652fc3490749e9e87c7c0e6
ipa3211 <kygsm2003@naver.com>
2020-02-22 17:51:31 +0900

    hello

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-#include "header/header.h"
+#include <algorithm>
+using namespace std;
 
-int main(void){
 
-    hello();
+int main() {
+    int input_array_size, input_queries;
 
-    return 0;
+    cin >> input_array_size;
+    cin >> input_queries;
+
+    int **a = new int *[input_array_size];
+
+    for(int i = 0; i < input_array_size; i++){
+        int k;
+        cin >> k;
+        a[i] = new int [k];
+
+        for (int j = 0; j < k; j++)
+            cin >> a[i][j];
+    }
+
+    for (int i = 0; i < input_queries; i++){
+        int input_query_x, input_query_y;
+
+        cin >> input_query_x;
+        cin >> input_query_y;
+
+        cout << a[input_query_x][input_query_y] << endl;
+    }
+
 }
